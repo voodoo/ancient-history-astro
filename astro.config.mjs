688e844ff0820e218423d2ed3372c7b1e5c299cf
Paul Vudmaska <paul@vudmaska.com>
2024-11-18 11:27:31 -0600

    fix update ai

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  base: '/ancient-history-astro',
+  site: 'https://voodoo.github.io'
 });
